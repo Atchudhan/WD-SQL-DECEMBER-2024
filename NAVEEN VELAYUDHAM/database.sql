461339686f5638d798d0436536407c69bfe0f0b9
naveen20052 <karunanaveen43@gmail.com>
2025-01-03 20:17:59 +0530

    Add files via upload

---
+++ NAVEEN VELAYUDHAM/database.sql
@@ -0,0 +1,15 @@
+ use naveen;
+create table naveen(
+   st_id  int,
+   lastname varchar(255),
+   firstname varchar(255),
+   city varchar(255)
+   );
+insert into naveen (st_id,lastname,firstname,city)values("456","ajith","vijay","chennai");
+insert into naveen(st_id,lastname,firstname,city)values("999","harish","kulla","pondy");
+insert into naveen(st_id,lastname,firstname,city)values("777","ajay","kumar","africa");
+insert into naveen(st_id,lastname,firstname,city)values("666","deepak","raj","salem");
+insert into naveen(st_id,lastname,firstname,city)values("888","raj","kumar","france");
+select*from naveen;
+   
+   
