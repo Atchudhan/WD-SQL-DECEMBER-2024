839506a5fe11d80bd9a543778ecdf5203bef0ce9
Deva0804 <devanathan.jipmer@gmail.com>
2025-01-03 20:17:02 +0530

    V2

--- DEVANATHAN/FITA.sql
+++ DEVANATHAN/FITA.sql
@@ -55,3 +55,70 @@ Insert into Jipmer2(Caseid,Patientname,Mobilenumber,Department,Address,NextVisit
                   (1001,"Harish",8248457194,"Opthal","Pondicherry","Friday","NIL"),
                   (1002,"Berna",9085214785,"Dental","Pondicherry","Friday","NIL"),
                   (1003,"Satya",8524785693,"Anes","Pondicherry","Friday","NIL");
+                  
+ select Patientname,Department,Diagnosis from Jipmer2
+ 
+ SELECT Patientname,Department,Mobilenumber+Address AS Final from Jipmer2;
+ 
+ create table Orders(OrderdetailsID int,OrderID bigint,ProductName varchar(50),Quantity int); 
+ 
+ Insert into Orders(OrderdetailsID,OrderID,ProductName,Quantity)
+ values(1,10248,"Tea",5),
+	   (2,10249,"Coffee",6),
+       (3,10250,"Biscuit",7),
+       (4,10251,"Bytes",8);
+       
+SELECT Quantity AS Quan
+FROM Orders;
+
+SELECT OrderID
+FROM Orders AS Request;
+       
+ select * from Orders; 
+ 
+ update Orders 
+ set ProductName="Tea"
+ where OrderdetailsID=1;
+ 
+ update Orders
+ set Quantity = 10
+ where OrderdetailsID=1;
+ 
+ update Orders set ProductName="3Rose",Quantity=15 where OrderdetailsID=1;
+ 
+ Delete from Orders;
+ 
+ Delete from Orders where OrderID=10248;
+ select * from Orders; 
+ 
+ select * from Orders where ProductName LIKE 'c%';
+  select * from Orders where ProductName LIKE '%t';
+
+ select * from Orders where ProductName IN ('Coffee');
+ select * from Orders where ProductName IN ('Coffee','Bytes');
+ 
+   select * from Orders where ProductName NOT LIKE '%t';
+   select * from Orders where ProductName NOT LIKE '%t';
+   
+   select * from Orders where Quantity BETWEEN 6 and 8;
+      select * from Orders where Quantity NOT BETWEEN 6 and 8;
+      
+ select * from Orders where Quantity BETWEEN 6 and 8 AND OrderID IN ('10250');
+ select * from Orders where Quantity NOT BETWEEN 6 and 7 AND OrderID IN ('10250');
+
+   
+   
+   
+   
+   
+   
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+                  
+                  
